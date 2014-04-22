2db9f1da557282dc0f1c630336cf63cbb0429958
Zongmin Lei <leizongmin@gmail.com>
2014-04-22 12:35:33 +0800

    admin article edit 未完成

--- src/default/router/admin/article.js
+++ src/default/router/admin/article.js
@@ -8,6 +8,9 @@ module.exports = function (ns, router) {
   var async = require('async');
   var app = ns('app');
   var checkSignin = ns('middleware.check_signin');
+  var csrf = ns('middleware.csrf');
+  var multiparty = ns('middleware.multiparty');
+
 
   router.get('/admin/article/list', checkSignin, function (req, res, next) {
     async.series([
@@ -29,6 +32,26 @@ module.exports = function (ns, router) {
     ], function (err) {
       if (err) return next(err);
       res.render('admin/article/list');
+    });
+  });
+
+
+  router.get('/admin/article/:id/edit', checkSignin, csrf, function (req, res, next) {
+    console.log(req.params);
+    app.call('article.get', {id: req.params.id}, function (err, article) {
+      if (err) return next(err);
+
+      res.setLocals('article', article);
+      res.render('admin/article/edit');
+    });
+  });
+
+  router.post('/admin/article/:id/edit', checkSignin, multiparty, csrf, function (req, res, next) {
+    req.body.id = req.params.id;
+    app.call('article.update', req.body, function (err, ret) {
+      if (err) return next(err);
+
+      res.redirect(req.url);
     });
   });
 
