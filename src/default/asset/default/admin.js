037f3397db2d41196bec7e6924727536f3de0891
Zongmin Lei <leizongmin@gmail.com>
2014-04-23 10:44:13 +0800

    文章编辑页面，标签输入框

--- src/default/asset/default/admin.js
+++ src/default/asset/default/admin.js
@@ -1,1 +1,15 @@
 /* Peento default JavaScript */
+$(document).ready(function () {
+
+   // init tag-it
+  $('input[role="tag-it"]').each(function () {
+    var $me = $(this);
+    var tags = $me.data('tags');
+    if (!tags) tags = '';
+    tags = tags.split(',');
+    $me.tagit({
+      availableTags: tags
+    });
+  });
+
+});
