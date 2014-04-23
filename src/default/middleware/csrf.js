671441de87b4c0b5e032b85f54d9a3d9c15dfdc9
Zongmin Lei <leizongmin@gmail.com>
2014-04-23 10:19:01 +0800

    Fixed default.middleware.csrf

--- src/default/middleware/csrf.js
+++ src/default/middleware/csrf.js
@@ -6,16 +6,15 @@
 
 module.exports = function (ns, debug) {
 
-  var express = require('express');
   var csurf = require('csurf');
 
-  var router = express.Router();
-  router.use(csurf());
-  router.use(function (req, res, next) {
-    res.context.setLocals('_csrf', req.csrfToken());
-    next();
-  });
-
-  return router;
+  var csrf = csurf();
+  return function (req, res, next) {
+    csrf(req, res, function (err) {
+      if (err) return next(err);
+      res.context.setLocals('_csrf', req.csrfToken());
+      next();
+    });
+  };
 
 };
